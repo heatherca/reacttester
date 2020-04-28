7cdbf12e2a1be7c4f73897a052223ac9f28473dc
heatherca <h.c.andreadis@gmail.com>
2020-04-28 10:56:02 -0400

    Updates

---
+++ precache-manifest.8c47d04d6c44855d4ee597b89307cadf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9a3e43e564d596ff7724a3e6e56f6c",
+    "url": "/reacttester/index.html"
+  },
+  {
+    "revision": "dfe2f05071cab9180a6f",
+    "url": "/reacttester/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cbf297c83524ab4f2351",
+    "url": "/reacttester/static/js/2.25136a90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttester/static/js/2.25136a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe2f05071cab9180a6f",
+    "url": "/reacttester/static/js/main.d3bf2cc4.chunk.js"
+  },
+  {
+    "revision": "74602b010efd1c6d854d",
+    "url": "/reacttester/static/js/runtime-main.23a9bfc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttester/static/media/logo.5d5d9eef.svg"
+  }
+]);
